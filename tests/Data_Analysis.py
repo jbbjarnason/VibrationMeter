7bf22fe12b4074001840e8144f63e92b1378f98e
Jon Bjarni Bjarnason <jbbjarnason@marel.com>
2018-10-13 17:36:06 +0000

    created class object for data analysis

---
+++ tests/Data_Analysis.py
@@ -0,0 +1,5 @@
+
+
+class DataAnalysis:
+    def __init__(self):
+        pass
---
+++ tests/test_DataAnalysis.py
@@ -0,0 +1,11 @@
+import unittest
+from mock import MagicMock
+from Data_Collection import DataCollection
+
+class Test_DataAnalysis(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_provideInstance(self):
+        self.assertEqual(1, 1)
