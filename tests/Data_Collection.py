625b24b030f309843ab67699ace1f6e1e4200d94
Jon Bjarni Bjarnason <jbbjarnason@marel.com>
2018-10-13 17:01:31 +0000

    Refactor private vars

--- tests/Data_Collection.py
+++ tests/Data_Collection.py
@@ -3,32 +3,32 @@ import time
 import numpy as np
 class DataCollection:
     def __init__(self, instance=None, interval_ms=None, span_s=None, callback=None):
-        self.instance       = instance if instance != None else 1
-        self.interval_ms    = interval_ms if interval_ms != None else 1000
-        self.span_s         = span_s if span_s != None else 60
+        self._instance       = instance if instance != None else 1
+        self._interval_ms    = interval_ms if interval_ms != None else 1000
+        self._span_s         = span_s if span_s != None else 60
         if not callable(callback): raise Exception("Sample function not provided")
-        self.getData        = callback
-        self.measurements   = np.array([])
-        self.intervalTime   = self.interval_ms/1000.0
-        self.timer          = threading.Timer(self.intervalTime, self._timerInterval)
+        self._getData        = callback
+        self._measurements   = np.array([])
+        self._intervalTime   = self._interval_ms/1000.0
+        self._timer          = threading.Timer(self._intervalTime, self._timerInterval)
     
     def _timerInterval(self):
         currTime = time.time()
-        self.measurements = np.append(self.measurements, 
-                            Measurement(data=self.getData(), 
+        self._measurements = np.append(self._measurements, 
+                            Measurement(data=self._getData(), 
                                         time=currTime))
         self._tryEraseOldestData(currTime)
-        self.timer.start()
+        self._timer.start()
     def _tryEraseOldestData(self, currTime):
-        oldTime = currTime - self.span_s
-        if (self.measurements[0].time <= oldTime):
-            self.measurements = np.delete(self.measurements, 0)
+        oldTime = currTime - self._span_s
+        if (self._measurements[0].time <= oldTime):
+            self._measurements = np.delete(self._measurements, 0)
     def startSampling(self):
         self._timerInterval()
     def stopSampling(self):
-        self.timer.stop()
+        self._timer.stop()
     def getMeasurements(self):
-        return self.measurements
+        return self._measurements
 
 
 class Measurement:
--- tests/test_DataCollection.py
+++ tests/test_DataCollection.py
@@ -17,35 +17,35 @@ class Test_DataCollection(unittest.TestCase):
                                             interval_ms=self.myInterval, 
                                             span_s=self.mySpan,
                                             callback=self.sample)
-        self.myInstance.timer.start = MagicMock()
-        self.myInstance.timer.stop = MagicMock()
+        self.myInstance._timer.start = MagicMock()
+        self.myInstance._timer.stop = MagicMock()
 
     def test_provideInstance(self):
         for i in range(1,10):
             testInstance = DataCollection(callback=self.sample, instance=i)
-            self.assertEqual(testInstance.instance,i)
+            self.assertEqual(testInstance._instance,i)
     
     def test_defaultInstanceIsOne(self):
         testInstance = DataCollection(callback=self.sample)
-        self.assertEqual(testInstance.instance,1)
+        self.assertEqual(testInstance._instance,1)
 
     def test_provideInterval(self):
-        self.assertEqual(self.myInstance.interval_ms,self.myInterval)
+        self.assertEqual(self.myInstance._interval_ms,self.myInterval)
 
     def test_defaultIntervalOneSec(self):
         testInstance = DataCollection(callback=self.sample)
-        self.assertEqual(testInstance.interval_ms,1000)
+        self.assertEqual(testInstance._interval_ms,1000)
 
     def test_providesDataSpan(self):
-        self.assertEqual(self.myInstance.span_s,self.mySpan)
+        self.assertEqual(self.myInstance._span_s,self.mySpan)
 
     def test_defaultIntervalOneSec(self):
         testInstance = DataCollection(callback=self.sample)
-        self.assertEqual(testInstance.span_s,60)
+        self.assertEqual(testInstance._span_s,60)
 
     def test_provideSampleFunction(self):
         testInstance = DataCollection(callback=self.sample)
-        self.assertTrue(callable(testInstance.getData))
+        self.assertTrue(callable(testInstance._getData))
 
     def test_defaultSampleFunctionRaiseError(self):
         with self.assertRaises(Exception): DataCollection()
@@ -58,50 +58,50 @@ class Test_DataCollection(unittest.TestCase):
 
     def test_startDataSampling(self):
         self.myInstance.startSampling()
-        self.myInstance.timer.start.assert_called_with()
+        self.myInstance._timer.start.assert_called_with()
 
     def test_stopDataSampling(self):
         self.myInstance.stopSampling()
-        self.myInstance.timer.stop.assert_called_with()
+        self.myInstance._timer.stop.assert_called_with()
 
     def test_onTimerInterruptAppendToData(self):
         self.myInstance._timerInterval()
-        self.assertEqual(len(self.myInstance.measurements), 1)
+        self.assertEqual(len(self.myInstance._measurements), 1)
 
     def test_onTimerInterruptStoreFetchedData(self):
-        self.myInstance.getData = MagicMock(return_value=42)
+        self.myInstance._getData = MagicMock(return_value=42)
         self.myInstance._timerInterval()
-        self.assertEqual(self.myInstance.measurements[0].data, 42)
+        self.assertEqual(self.myInstance._measurements[0].data, 42)
 
     def test_onTimerInterruptStoreTimeStamp(self):
         time.time = MagicMock(return_value=43)
         self.myInstance._timerInterval()
-        self.assertEqual(self.myInstance.measurements[0].time, 43)
+        self.assertEqual(self.myInstance._measurements[0].time, 43)
 
     def test_onTimerInterruptStoreAmountOfData(self):
         often = 9
         for i in range(0,often):
             myData = random.uniform(0, 1456315)
             myTime = i
-            self.myInstance.getData = MagicMock(return_value=myData)
+            self.myInstance._getData = MagicMock(return_value=myData)
             time.time = MagicMock(return_value=myTime)
             self.myInstance._timerInterval()
-            self.assertEqual(self.myInstance.measurements[i].data, myData)
-            self.assertEqual(self.myInstance.measurements[i].time, myTime)
-        self.assertEqual(len(self.myInstance.measurements), often)
+            self.assertEqual(self.myInstance._measurements[i].data, myData)
+            self.assertEqual(self.myInstance._measurements[i].time, myTime)
+        self.assertEqual(len(self.myInstance._measurements), often)
 
     def test_capMeasurementsSize(self): 
-        self.myInstance.span_s = 50
-        often = self.myInstance.span_s * 2
+        self.myInstance._span_s = 50
+        often = self.myInstance._span_s * 2
         for i in range(0,often):
-            self.myInstance.getData = MagicMock(return_value=i)
+            self.myInstance._getData = MagicMock(return_value=i)
             time.time = MagicMock(return_value=i)
             self.myInstance._timerInterval()
-        self.assertEqual(len(self.myInstance.measurements), self.myInstance.span_s)
-        self.assertEqual(self.myInstance.measurements[0].data, 50)
+        self.assertEqual(len(self.myInstance._measurements), self.myInstance._span_s)
+        self.assertEqual(self.myInstance._measurements[0].data, 50)
 
     def test_getCurrentData(self):
-        pointer, read_only_flag = self.myInstance.measurements.__array_interface__['data']
+        pointer, read_only_flag = self.myInstance._measurements.__array_interface__['data']
         returnPointer, read_only_flag = self.myInstance.getMeasurements().__array_interface__['data']
         self.assertEqual(returnPointer, pointer)
 
