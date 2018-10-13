9d602806a8d70e056372102ccd9e21c02585d402
Jon Bjarni Bjarnason <jbbjarnason@marel.com>
2018-10-13 15:21:21 +0000

    Erase oldest data if span of data is reached

--- tests/Data_Collection.py
+++ tests/Data_Collection.py
@@ -2,9 +2,10 @@ import threading
 import time
 import numpy as np
 class DataCollection:
-    def __init__(self, instance=None, interval_ms=None, callback=None):
+    def __init__(self, instance=None, interval_ms=None, span_s=None, callback=None):
         self.instance       = instance if instance != None else 1
         self.interval_ms    = interval_ms if interval_ms != None else 1000
+        self.span_s         = span_s if span_s != None else 60
         if not callable(callback): raise Exception("Sample function not provided")
         self.getData        = callback
         self.measurements   = np.array([])
@@ -12,9 +13,16 @@ class DataCollection:
         self.timer          = threading.Timer(self.intervalTime, self._timerInterval)
     
     def _timerInterval(self):
+        currTime = time.time()
         self.measurements = np.append(self.measurements, 
-                            Measurement(data=self.getData(), time=time.time()))
+                            Measurement(data=self.getData(), 
+                                        time=currTime))
+        self._tryEraseOldestData(currTime)
         self.timer.start()
+    def _tryEraseOldestData(self, currTime):
+        oldTime = currTime - self.span_s
+        if (self.measurements[0].time <= oldTime):
+            self.measurements = np.delete(self.measurements, 0)
     def startSampling(self):
         self._timerInterval()
     def stopSampling(self):
@@ -24,4 +32,9 @@ class Measurement:
     def __init__(self, data, time):
         self.data = data
         self.time = time
+
+    # def __trunc__(self):
+    #     del self.data
+    #     del self.time
+        # del self
 
--- tests/test_DataCollection.py
+++ tests/test_DataCollection.py
@@ -10,9 +10,13 @@ class Test_DataCollection(unittest.TestCase):
     sample = lambda x:0
     myInstanceNumber = 42
     myInterval = 1234 
+    mySpan = 42
     myInstance = 0 
     def setUp(self):
-        self.myInstance = DataCollection(instance=self.myInstanceNumber, interval_ms=self.myInterval, callback=self.sample)
+        self.myInstance = DataCollection(   instance=self.myInstanceNumber, 
+                                            interval_ms=self.myInterval, 
+                                            span_s=self.mySpan,
+                                            callback=self.sample)
         self.myInstance.timer.start = MagicMock()
         self.myInstance.timer.stop = MagicMock()
 
@@ -31,6 +35,13 @@ class Test_DataCollection(unittest.TestCase):
     def test_defaultIntervalOneSec(self):
         testInstance = DataCollection(callback=self.sample)
         self.assertEqual(testInstance.interval_ms,1000)
+
+    def test_providesDataSpan(self):
+        self.assertEqual(self.myInstance.span_s,self.mySpan)
+
+    def test_defaultIntervalOneSec(self):
+        testInstance = DataCollection(callback=self.sample)
+        self.assertEqual(testInstance.span_s,60)
 
     def test_provideSampleFunction(self):
         testInstance = DataCollection(callback=self.sample)
@@ -79,7 +90,17 @@ class Test_DataCollection(unittest.TestCase):
             self.assertEquals(self.myInstance.measurements[i].time, myTime)
         self.assertEquals(len(self.myInstance.measurements), often)
 
-    # def test_capMeasurementsSize(self): create span argument to datacollection
+    def test_capMeasurementsSize(self): 
+        self.myInstance.span_s = 50
+        often = self.myInstance.span_s * 2
+        for i in range(0,often):
+            self.myInstance.getData = MagicMock(return_value=i)
+            time.time = MagicMock(return_value=i)
+            self.myInstance._timerInterval()
+        self.assertEquals(len(self.myInstance.measurements), self.myInstance.span_s)
+        self.assertEquals(self.myInstance.measurements[0].data, 50)
+
+        # create span argument to datacollection
 
 def getRandomFloat():
     return random.uniform(0, 1456315)
