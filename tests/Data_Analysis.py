6419a74f3ef26ff61d97417c2b9c77a9c3d8d995
Jon Bjarni Bjarnason <jbbjarnason@marel.com>
2018-10-16 22:06:27 +0000

    fourier transformation tested

--- tests/Data_Analysis.py
+++ tests/Data_Analysis.py
@@ -1,4 +1,5 @@
-from scipy.fftpack import fft, ifft, fftfreq
+import scipy.fftpack
+# from scipy.fftpack import fft, ifft, fftfreq
 import numpy as np
 import random
 
@@ -7,17 +8,23 @@ class DataAnalysis:
                  lowPassFreq=None, highPassFreq=None, integralOrder=None):
         self._instance       = instance if instance != None else 1
 
-    def analyze(self, dictMeasurements, samplingPeriod): # data should be a dictionary including {"data":array, "time":array}
+    def analyze(self, dictMeasurements): # data should be a dictionary including {"data":array, "time":array}
         signal = dictMeasurements["data"]
-        numberOfSamples = len(signal)
-        fourierOfSignal = fft(signal)
-        freq = fftfreq(numberOfSamples, samplingPeriod)
-        import matplotlib.pyplot as plt
-        f = freq[:numberOfSamples // 2]
-        s = abs(fourierOfSignal[:numberOfSamples // 2])
-        plt.plot(f, s)
-        plt.grid()
-        plt.show()
+        time = dictMeasurements["time"]
+        fourier = scipy.fftpack.fft(signal)
+        samplingFreq = self.tryGetSamplingFreq(time)
+        freq = scipy.fftpack.fftfreq(signal.size, samplingFreq)
+        return {"signal":signal, "time":time,
+                "fourier":fourier, "freq":freq}
+        # import matplotlib.pyplot as plt
+        # f = freq[:numberOfSamples // 2]
+        # s = abs(fourierOfSignal[:numberOfSamples // 2])
+        # plt.plot(f, s)
+        # plt.grid()
+        # plt.show()
+
+    def tryGetSamplingFreq(self, timeArray): # Hz
+        pass
 
 if __name__ == '__main__':
     analysis = DataAnalysis()
--- tests/tests_DataAnalysis.py
+++ tests/tests_DataAnalysis.py
@@ -1,10 +1,19 @@
 import unittest
+import numpy as np
 from mock import MagicMock
 from Data_Analysis import DataAnalysis
+import scipy.fftpack
+# from scipy.fftpack import fft, ifft, fftfreq
+fftReturnVal = "fft called"
+fftFreqReturnVal = "fftfreq called"
+scipy.fftpack.fft = MagicMock(return_value=fftReturnVal)
+scipy.fftpack.fftfreq = MagicMock(return_value=fftFreqReturnVal)
 
 class Test_DataAnalysis(unittest.TestCase):
     getData = lambda x:0
     myInstanceNumber = 42
+    testData = {"data":np.array([1.2,12,465,3,4,698,41,1,84,6,1,85]),
+                "time": np.array([1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10,1.11])}
     def setUp(self):
         self.myInstance = DataAnalysis(instance=self.myInstanceNumber,
                                        callback=self.getData)
@@ -17,8 +26,25 @@ class Test_DataAnalysis(unittest.TestCase):
         self.assertEqual(testInstance._instance,1)
 
     def test_fourierTransformData(self):
-        pass
+        # self.myInstance.tryGetSamplingPeriod = MagicMock(return_value=0.1)
+        self.myInstance.analyze(self.testData)
+        scipy.fftpack.fft.assert_called_with(self.testData["data"])
+    def test_getFreqForFourierTransfor(self):
+        samplingFreq = 10 # Hz
+        self.myInstance.tryGetSamplingFreq = MagicMock(return_value=samplingFreq)
+        self.myInstance.analyze(self.testData)
+        self.myInstance.tryGetSamplingFreq.assert_called_with(self.testData["time"])
+        scipy.fftpack.fftfreq.assert_called_with(self.testData["data"].size, samplingFreq)
 
+    def test_returnFourierData(self):
+        self.myInstance.tryGetSamplingFreq = MagicMock(return_value=10)
+        self.assertEqual(self.myInstance.analyze(self.testData), {"signal":self.testData["data"],
+                                                                  "time":self.testData["time"],
+                                                                  "fourier":fftReturnVal,
+                                                                  "freq":fftFreqReturnVal})
+
+    def test_getSamplingPeriod(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
