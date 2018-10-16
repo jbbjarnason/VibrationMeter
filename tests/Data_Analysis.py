9abbc23dfc2b12f9cf66bcb855bd6d3767f91116
Jon Bjarni Bjarnason <jbbjarnason@marel.com>
2018-10-16 21:32:45 +0000

    test of plotting fft finished, needs to develop generic way of analysing data

--- tests/Data_Analysis.py
+++ tests/Data_Analysis.py
@@ -1,4 +1,4 @@
-from scipy.fftpack import fft, ifft
+from scipy.fftpack import fft, ifft, fftfreq
 import numpy as np
 import random
 
@@ -7,34 +7,17 @@ class DataAnalysis:
                  lowPassFreq=None, highPassFreq=None, integralOrder=None):
         self._instance       = instance if instance != None else 1
 
-    def analyze(self, dictMeasurements, samplingFreq): # data should be a dictionary including {"data":array, "time":array}
-        data = dictMeasurements["data"]
-        numberOfSamples = len(data)
-        xAxisTimeDomain = np.linspace(0, numberOfSamples*samplingFreq, numberOfSamples) # for testing
-        dataFrequencyDomain = fft(data)
-        xAxisFreqDomain = np.linspace(0.0, 1.0/(2.0*samplingFreq), numberOfSamples//2)
+    def analyze(self, dictMeasurements, samplingPeriod): # data should be a dictionary including {"data":array, "time":array}
+        signal = dictMeasurements["data"]
+        numberOfSamples = len(signal)
+        fourierOfSignal = fft(signal)
+        freq = fftfreq(numberOfSamples, samplingPeriod)
         import matplotlib.pyplot as plt
-        plt.plot(xAxisFreqDomain, 2.0 / numberOfSamples * np.abs(dataFrequencyDomain[0:numberOfSamples // 2]))
+        f = freq[:numberOfSamples // 2]
+        s = abs(fourierOfSignal[:numberOfSamples // 2])
+        plt.plot(f, s)
         plt.grid()
         plt.show()
-
-# import numpy as np
-# from scipy.fftpack import fft
-# # Number of sample points
-# N = 600
-# # sample spacing
-# T = 1.0 / 800.0
-# x = np.linspace(0.0, N*T, N)
-# y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
-# yf = fft(y)
-# xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
-# import matplotlib.pyplot as plt
-# plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
-# plt.grid()
-#
-#
-# plt.show()
-
 
 if __name__ == '__main__':
     analysis = DataAnalysis()
