3a626a8774467ec2a2457b4fdb68b78ac4198567
jbbjarnason <jbbjarnason@gmail.com>
2018-10-27 21:41:14 +0000

    binary sensor interface

---
+++ tests/Dual_Sensor.py
@@ -0,0 +1,9 @@
+from Data_Collection import DataCollection
+
+class DualSensor:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass
+    
---
+++ tests/test_DualSensor.py
@@ -0,0 +1,8 @@
+import unittest
+from Dual_Sensor import DualSensor
+from mock import MagicMock
+
+
+class Test_DualSensor(unittest.TestCase):
+    def setUp(self):
+        pass
