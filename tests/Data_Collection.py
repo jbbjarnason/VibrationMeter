ff51d4a38d7fe01cba5c28f1059c846d88879cbd
Jón Bjarni <jbbjarnason@gmail.com>
2018-10-28 22:55:41 +0000

    Splitted Data collection to periodic as inherited class

--- tests/Data_Collection.py
+++ tests/Data_Collection.py
@@ -1,58 +1,29 @@
-import threading
-import time
 import numpy as np
 import logging
 class DataCollection:
-    def __init__(self, instance=None, interval_ms=None, span_s=None, callback=None):
+    def __init__(self, instance=None, period_s=None):
         self._instance       = instance if instance != None else 1
-        self._interval_ms    = interval_ms if interval_ms != None else 1000
-        self._span_s         = span_s if span_s != None else 60
-        if not callable(callback): raise Exception("Sample function not provided")
-        self._getData        = callback
+        self._period_s         = period_s if period_s != None else 60
         self._measurements   = Measurements()
-        self._setInterval()
-        self._timer          = threading.Timer(self._intervalTime, self._timerInterval)
         self.logger = logging.getLogger("DataCollection."+str(self._instance))
         self.logger.setLevel(logging.INFO)
-        mess =  "Instance created of data collection with interval " \
-                +str(self._intervalTime) + " sec, and span set as" \
-                                           +str(self._span_s) + " sec."
+        mess =  "Instance created of data collection with time period " \
+                                           +str(self._period_s) + " sec."
         self.logger.info(msg=mess)
-
-    def _timerInterval(self):
-        currTime = time.time()
-        self._measurements.append(self._getData(), currTime)
-        self._tryEraseOldestData(currTime)
-        self._timer.start()
     def _tryEraseOldestData(self, currTime):
-        oldTime = currTime - self._span_s
-        if (self._measurements.tryGetFirstTime()<= oldTime):
-            self.logger.debug(msg="Erase measurement from time "+str(self._measurements.time[0])\
-                              +" at time "+str(currTime))
+        oldTime = currTime - self._period_s
+        if (self._measurements.tryGetFirstTime() <= oldTime):
+            self.logger.debug(msg="Erase measurement from time " + str(self._measurements.time[0]) \
+                                  + " at time " + str(currTime))
             self._measurements.delete(0)
-    def _setInterval(self):
-        self._intervalTime = self._interval_ms/1000.0
-    def startSampling(self):
-        self._timerInterval()
-    def stopSampling(self):
-        self._timer.stop()
     def getData(self):
         self.logger.info(msg="Returning "+str(len(self._measurements))+ " items of data")
         return {"data":self._measurements.data, "time":self._measurements.time}
-    def getSpan(self):
-        return self._span_s
-    def changeSpan(self, s=None):
-        self._span_s = s if s != None else self._span_s
-        self.logger.info(msg="Span changed to "+str(self._span_s)+ " sec")
-    def getInterval(self):
-        return self._interval_ms
-    def changeInterval(self, ms=None):
-        self._interval_ms = ms if ms != None else self._interval_ms
-        self._setInterval()
-        self.logger.info(msg="Interval changed to "+str(self._intervalTime)+ " sec")
-        self.stopSampling()
-        self.startSampling()
-
+    def getPeriod(self):
+        return self._period_s
+    def changePeriod(self, s=None):
+        self._period_s = s if s != None else self._period_s
+        self.logger.info(msg="Period changed to " + str(self._period_s) + " sec")
 
 class Measurements:
     def __init__(self):
--- tests/test_DataCollection.py
+++ tests/test_DataCollection.py
@@ -4,54 +4,54 @@ import time
 import random
 from mock import MagicMock
 threading.Timer = MagicMock()
-from Data_Collection import DataCollection
+from Data_Collection_Periodic import DataCollectionPeriodic
 
 class Test_DataCollection(unittest.TestCase):
     sample = lambda x:0
     myInstanceNumber = 42
     myInterval = 1234 
-    mySpan = 42
+    myPeriod = 42
     myInstance = 0 
     def setUp(self):
-        self.myInstance = DataCollection(   instance=self.myInstanceNumber, 
+        self.myInstance = DataCollectionPeriodic(   instance=self.myInstanceNumber,
                                             interval_ms=self.myInterval, 
-                                            span_s=self.mySpan,
+                                            period_s=self.myPeriod,
                                             callback=self.sample)
         self.myInstance._timer.start = MagicMock()
         self.myInstance._timer.stop = MagicMock()
 
     def test_provideInstance(self):
         for i in range(1,10):
-            testInstance = DataCollection(callback=self.sample, instance=i)
+            testInstance = DataCollectionPeriodic(callback=self.sample, instance=i)
             self.assertEqual(testInstance._instance,i)
     
     def test_defaultInstanceIsOne(self):
-        testInstance = DataCollection(callback=self.sample)
+        testInstance = DataCollectionPeriodic(callback=self.sample)
         self.assertEqual(testInstance._instance,1)
 
     def test_provideInterval(self):
         self.assertEqual(self.myInstance._interval_ms,self.myInterval)
 
     def test_defaultIntervalOneSec(self):
-        testInstance = DataCollection(callback=self.sample)
+        testInstance = DataCollectionPeriodic(callback=self.sample)
         self.assertEqual(testInstance._interval_ms,1000)
 
-    def test_providesDataSpan(self):
-        self.assertEqual(self.myInstance._span_s,self.mySpan)
+    def test_providesDataPeriod(self):
+        self.assertEqual(self.myInstance._period_s,self.myPeriod)
 
     def test_defaultIntervalOneSec(self):
-        testInstance = DataCollection(callback=self.sample)
-        self.assertEqual(testInstance._span_s,60)
+        testInstance = DataCollectionPeriodic(callback=self.sample)
+        self.assertEqual(testInstance._period_s,60)
 
     def test_provideSampleFunction(self):
-        testInstance = DataCollection(callback=self.sample)
+        testInstance = DataCollectionPeriodic(callback=self.sample)
         self.assertTrue(callable(testInstance._getData))
 
     def test_defaultSampleFunctionRaiseError(self):
-        with self.assertRaises(Exception): DataCollection()
+        with self.assertRaises(Exception): DataCollectionPeriodic()
 
     def test_raiseErrorIfCallbackIsNotCallable(self):
-        with self.assertRaises(Exception): DataCollection(callback=1)
+        with self.assertRaises(Exception): DataCollectionPeriodic(callback=1)
 
     def test_createTimer(self):
         threading.Timer.assert_called_with(self.myInterval/1000.0, self.myInstance._timerInterval)
@@ -91,13 +91,13 @@ class Test_DataCollection(unittest.TestCase):
         self.assertEqual(len(self.myInstance._measurements.data), often)
 
     def test_capMeasurementsSize(self): 
-        self.myInstance._span_s = 50
-        often = self.myInstance._span_s * 2
+        self.myInstance._period_s = 50
+        often = self.myInstance._period_s * 2
         for i in range(0,often):
             self.myInstance._getData = MagicMock(return_value=i)
             time.time = MagicMock(return_value=i)
             self.myInstance._timerInterval()
-        self.assertEqual(len(self.myInstance._measurements), self.myInstance._span_s)
+        self.assertEqual(len(self.myInstance._measurements), self.myInstance._period_s)
         self.assertEqual(self.myInstance._measurements.data[0], 50)
 
     def test_getCurrentData(self):
@@ -105,15 +105,15 @@ class Test_DataCollection(unittest.TestCase):
         returnPointer, read_only_flag = self.myInstance.getData()["data"].__array_interface__['data']
         self.assertEqual(returnPointer, pointer)
 
-    def test_getSpan(self):
-        self.assertEqual(self.myInstance._span_s, self.myInstance.getSpan())
-    def test_changeSpan(self):
-        self.myInstance.changeSpan(5461)
-        self.assertEqual(self.myInstance._span_s, 5461)
-    def test_onlyChangeSpanIfProvided(self):
-        lastSpan = self.myInstance._span_s
-        self.myInstance.changeSpan()
-        self.assertEqual(self.myInstance._span_s, lastSpan)
+    def test_getPeriod(self):
+        self.assertEqual(self.myInstance._period_s, self.myInstance.getPeriod())
+    def test_changePeriod(self):
+        self.myInstance.changePeriod(5461)
+        self.assertEqual(self.myInstance._period_s, 5461)
+    def test_onlyChangePeriodIfProvided(self):
+        lastPeriod = self.myInstance._period_s
+        self.myInstance.changePeriod()
+        self.assertEqual(self.myInstance._period_s, lastPeriod)
 
     def test_getInterval(self):
         self.assertEqual(self.myInstance._interval_ms, self.myInstance.getInterval())
