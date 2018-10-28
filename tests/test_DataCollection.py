7c4d9586c414f82a766aed38bcf02409d4038e07
Jón Bjarni <jbbjarnason@gmail.com>
2018-10-28 23:10:48 +0000

    Splitted tests for inheritance

--- tests/test_DataCollection.py
+++ tests/test_DataCollection.py
@@ -1,10 +1,8 @@
 import unittest
-import threading
 import time
 import random
 from mock import MagicMock
-threading.Timer = MagicMock()
-from Data_Collection_Periodic import DataCollectionPeriodic
+from Data_Collection import DataCollection
 
 class Test_DataCollection(unittest.TestCase):
     sample = lambda x:0
@@ -13,83 +11,26 @@ class Test_DataCollection(unittest.TestCase):
     myPeriod = 42
     myInstance = 0 
     def setUp(self):
-        self.myInstance = DataCollectionPeriodic(   instance=self.myInstanceNumber,
-                                            interval_ms=self.myInterval, 
-                                            period_s=self.myPeriod,
-                                            callback=self.sample)
-        self.myInstance._timer.start = MagicMock()
-        self.myInstance._timer.stop = MagicMock()
+        self.myInstance = DataCollection(   instance=self.myInstanceNumber,
+                                            period_s=self.myPeriod)
 
     def test_provideInstance(self):
         for i in range(1,10):
-            testInstance = DataCollectionPeriodic(callback=self.sample, instance=i)
+            testInstance = DataCollection(instance=i)
             self.assertEqual(testInstance._instance,i)
     
     def test_defaultInstanceIsOne(self):
-        testInstance = DataCollectionPeriodic(callback=self.sample)
+        testInstance = DataCollection()
         self.assertEqual(testInstance._instance,1)
-
-    def test_provideInterval(self):
-        self.assertEqual(self.myInstance._interval_ms,self.myInterval)
-
-    def test_defaultIntervalOneSec(self):
-        testInstance = DataCollectionPeriodic(callback=self.sample)
-        self.assertEqual(testInstance._interval_ms,1000)
 
     def test_providesDataPeriod(self):
         self.assertEqual(self.myInstance._period_s,self.myPeriod)
 
-    def test_defaultIntervalOneSec(self):
-        testInstance = DataCollectionPeriodic(callback=self.sample)
+    def test_defaultPeriodOneMin(self):
+        testInstance = DataCollection()
         self.assertEqual(testInstance._period_s,60)
 
-    def test_provideSampleFunction(self):
-        testInstance = DataCollectionPeriodic(callback=self.sample)
-        self.assertTrue(callable(testInstance._getData))
-
-    def test_defaultSampleFunctionRaiseError(self):
-        with self.assertRaises(Exception): DataCollectionPeriodic()
-
-    def test_raiseErrorIfCallbackIsNotCallable(self):
-        with self.assertRaises(Exception): DataCollectionPeriodic(callback=1)
-
-    def test_createTimer(self):
-        threading.Timer.assert_called_with(self.myInterval/1000.0, self.myInstance._timerInterval)
-
-    def test_startDataSampling(self):
-        self.myInstance.startSampling()
-        self.myInstance._timer.start.assert_called_with()
-
-    def test_stopDataSampling(self):
-        self.myInstance.stopSampling()
-        self.myInstance._timer.stop.assert_called_with()
-
-    def test_onTimerInterruptAppendToData(self):
-        self.myInstance._timerInterval()
-        self.assertEqual(len(self.myInstance._measurements), 1)
-
-    def test_onTimerInterruptStoreFetchedData(self):
-        self.myInstance._getData = MagicMock(return_value=42)
-        self.myInstance._timerInterval()
-        self.assertEqual(self.myInstance._measurements.data[0], 42)
-
-    def test_onTimerInterruptStoreTimeStamp(self):
-        time.time = MagicMock(return_value=43)
-        self.myInstance._timerInterval()
-        self.assertEqual(self.myInstance._measurements.time[0], 43)
-
-    def test_onTimerInterruptStoreAmountOfData(self):
-        often = 9
-        for i in range(0,often):
-            myData = random.uniform(0, 1456315)
-            myTime = i
-            self.myInstance._getData = MagicMock(return_value=myData)
-            time.time = MagicMock(return_value=myTime)
-            self.myInstance._timerInterval()
-            self.assertEqual(self.myInstance._measurements.data[i], myData)
-            self.assertEqual(self.myInstance._measurements.time[i], myTime)
-        self.assertEqual(len(self.myInstance._measurements.data), often)
-
+    @unittest.skip("Need to implement method in parent before this test")
     def test_capMeasurementsSize(self): 
         self.myInstance._period_s = 50
         often = self.myInstance._period_s * 2
@@ -114,25 +55,6 @@ class Test_DataCollection(unittest.TestCase):
         lastPeriod = self.myInstance._period_s
         self.myInstance.changePeriod()
         self.assertEqual(self.myInstance._period_s, lastPeriod)
-
-    def test_getInterval(self):
-        self.assertEqual(self.myInstance._interval_ms, self.myInstance.getInterval())
-    def test_changeInterval(self):
-        self.myInstance.changeInterval(465)
-        self.assertEqual(self.myInstance._interval_ms, 465)
-    def test_changeIntervalRestartsTimer(self):
-        self.myInstance._setInterval    = MagicMock()
-        self.myInstance.stopSampling    = MagicMock()
-        self.myInstance.startSampling   = MagicMock()
-        self.myInstance.changeInterval(465)
-        self.myInstance._setInterval.assert_called()
-        self.myInstance.stopSampling.assert_called()
-        self.myInstance.startSampling.assert_called()
-
-    def test_onlyChangeIntervalIfProvided(self):
-        last = self.myInstance._interval_ms
-        self.myInstance.changeInterval()
-        self.assertEqual(self.myInstance._interval_ms, last)
 
 
 if __name__ == '__main__':
